7a207379477a75dde8fde60fe9305c7a8c02a96e
mmadfox <sergey.liskonog@gmail.com>
2016-11-08 01:52:08 +0300

    added full example

--- examples/tracttv.go
+++ examples/tracttv.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/mmadfox/scraper"
 	"log"
 	"net/http"
 	"time"
 )
 
+type Movie struct {
+	Name   string `json: "name"`
+	Poster string `json: "poster"`
+}
+
 func main() {
 	log.Println("Trakt Tv scraper")
+	storage := make([]Movie, 0)
 	var wc scraper.WorkerCount = 5
 	h := http.Header{}
 	h.Add("Referer", "https://trakt.tv")
@@ -21,6 +28,12 @@ func main() {
 	s.Mux().HandleFunc(p, func(rw http.ResponseWriter, r *http.Request) {
 		ctx := rw.(*scraper.Context)
 		log.Println("Got the url", ctx.Addr.String())
+		name := ctx.Doc.Find(".mobile-title").Find("h1").Text()
+		poster, _ := ctx.Doc.Find("img.real").Attr("data-original")
+		storage = append(storage, Movie{
+			Name:   name,
+			Poster: poster,
+		})
 	})
 	s.SetHeader(h)
 	go func() {
@@ -34,4 +47,6 @@ func main() {
 	}()
 	s.StartAndWait()
 	log.Println("Stop")
+	js, _ := json.Marshal(storage)
+	log.Print(string(js))
 }
